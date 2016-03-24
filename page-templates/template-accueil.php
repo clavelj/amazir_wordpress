9c54feb29a3ec4f440732ed3a9d0ff76a1449a6b
clavelj <claveljeanne149@gmail.com>
2016-03-24 09:16:54 +0100

    responsive page accueil fin

--- page-templates/template-accueil.php
+++ page-templates/template-accueil.php
@@ -7,8 +7,8 @@
 <div class="col-md-8">
 	<div class="main page"> <?php if (have_posts()) : ?>
    <?php while (have_posts()) : the_post(); ?> 
-   <div class="post col-md-12 sectionaccueil">
-			<div class="col-md-7">
+   <div class="post col-md-12 col-sm-12 sectionaccueil">
+			<div class="col-md-7 col-sm-7">
 			<div class="titre1"><?php echo get_post_meta( get_the_ID(), '_accueil_titre_titre', true ) ?></div>
 			
 				<div class="titre2">
@@ -19,9 +19,9 @@
     			</div>
 			</div>
 			
-			<div class="col-md-5">
+			<div class="col-md-5 col-sm-5">
 				<?php the_post_thumbnail ( 'full',array (
-					'class' => 'img-responsive' 
+					'class' => 'img-responsive imagearticle' 
 				) );?>
 			</div>
 
@@ -43,18 +43,18 @@ $recentPosts->query ( 'showposts=2' );
 		<?php
 	if (has_post_thumbnail ()) { // pour afficher image a la une
 		?>
-			<div class="col-md-3">
+			<div class="col-md-3 col-sm-3 col-xs-12">
 				<?php
 		
 		the_post_thumbnail ( 'thumbnail', array (
-				'class' => 'img-responsive' 
+				'class' => 'img-responsive imagearticle' 
 		) );
 		?>
 			</div>
 		<?php
 	}
 	?>
-		<div class="col-md-9">
+		<div class="col-md-9 ">
 			<div class="titrearticle">
 				<a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title( "<h3 class='panel-title'>", "</h3>" ); ?></a>
 			</div>
