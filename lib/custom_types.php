b0bea290dfac4ce5f040546705a658f4d26736c7
clavelj <claveljeanne149@gmail.com>
2016-04-02 11:30:40 +0200

    colmd9 si image

--- lib/custom_types.php
+++ lib/custom_types.php
@@ -112,7 +112,7 @@ function bienetre_type() {
 			'show_ui'               => true,
 			'show_in_menu'          => true,
 			'menu_position'         => 10,
-			'menu_icon'             => 'dashicons-format-audio',
+			'menu_icon'             => 'dashicons-tag',
 			'show_in_admin_bar'     => true,
 			'show_in_nav_menus'     => true,
 			'can_export'            => true,
