3661396da385d2cb128f70b7654f3cfa2b3eedb3
unknown <clavelj@IUT1I-504-W14.iut1.local>
2016-04-07 09:26:35 +0200

    footer responsive ouiiii

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
 <?php global $redux_starter;?>
 </div>
 </div>
-<div class="post col-md-12 footercontent" style="">
+<div class="post col-md-12 footercontent  col-sm-12" style="">
 <footer id="footer" class="row">
-	<div class="col-md-2"></div>
-	<div class="col-md-8">
-		<div class="header-center col-md-3">
+	<div class="col-md-2  col-sm-2  hidden-xs"></div>
+	<div class="col-md-8  col-sm-8 col-xs-12">
+		<div class="header-center col-md-3  col-sm-3 footersmall">
 					<?php
 					wp_nav_menu ( array (
 							'menu' => 'secondary_navigation',
@@ -15,24 +15,24 @@
 					) );
 					?>
 					</div>
-		<div class="col-md-3 text-footer">
+		<div class="col-md-3  col-sm-3 text-footer footersmall">
 <?php echo $redux_starter ['text-footer1'];?>
 
  </div>
-		<div class="col-md-3 text-footer">
+		<div class="col-md-3 col-sm-3 text-footer footersmall">
 	<img class="img-responsive imgfooter center-block"
 				src="
 			<?php echo get_template_directory_uri(); ?>/images/usda.png"
 				alt="Logo Ecocert" />
 </div>
-		<div class="col-md-3">
+		<div class="col-md-3  col-sm-3 footersmall">
 			<img class="img-responsive imgfooter center-block"
 				src="
 			<?php echo get_template_directory_uri(); ?>/images/ecocert.png"
 				alt="Logo Ecocert" />
 		</div>
 	</div>
-	<div class="col-md-2"></div>
+	<div class="col-md-2  col-sm-2 hidden-xs"></div>
 </footer>
 </div>
 <?php wp_footer(); ?>
